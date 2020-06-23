864fa5f813588de4c7ec013abba8b37bd17ffb56
Carl Helmertz <helmertz@gmail.com>
2020-06-23 21:59:26 +0200

    Add readme with example usage

--- rofi_snippets.sh
+++ rofi_snippets.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SNIPDIR=${SNIPDIR:-~/snips}
 
-# fail early if deps are not to be found
+# fail early if deps are not found
 which xsel 2>&1 >/dev/null
 which xdotool 2>&1 >/dev/null
 test -d "$SNIPDIR"
@@ -14,16 +14,10 @@ if [ -z "$@" ]; then
 else
   file=$(ls -1 "$SNIPDIR/$1" 2>/dev/null)
 
-  # echo -n removes trailing newline, tr makes xsel understand newlines
+  # echo -n removes trailing newline,
+  #   tr makes xsel understand newlines
+  #     xsel -b uses the clipboard
   echo -n "$(cat "$file")" | tr '\n' '\r' | xsel -bi
 
-  # not sure what coproc does, stolen from
-  # https://github.com/raphaelfournier/rofi-modi-snippets
-
-
-  # another approach:
-  # xdotool type --clearmodifiers --file -
-  # wasn't really that optimized, even pretty short snippets (3 lines, 18 chars)
-  # took a long time to execute
-  coproc (xsel -bo | xdotool key ctrl+v)
+  coproc (xdotool key ctrl+v)
 fi
